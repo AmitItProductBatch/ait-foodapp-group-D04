2f4aba66425470d54b690949bc50c889769a8906
ruthvikpihulkar94 <ruthvikpihulkar@gmail.com>
2026-09-26 09:21:27 +0530

    Story OFD-134......Created OrderItem.java Entity

---
+++ food-delivery-backend/src/main/java/com/ait/app/model/OrderItem.java
@@ -0,0 +1,79 @@
+
+package com.ait.app.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "order_id")
+//    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id")
+    private MenuItem menuItem;
+
+    private String itemName;
+
+    private BigDecimal unitPrice;
+
+    private int quantity;
+
+    private BigDecimal subtotal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(MenuItem menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+}
+
